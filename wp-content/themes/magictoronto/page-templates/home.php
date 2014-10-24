5218aa3a41e63f0a2fb45d12a24f6ef9b35d1445
victoryland <faisal.ahmed0001@gmail.com>
2014-10-25 05:01:34 +0600

    Home page theme added

---
+++ wp-content/themes/magictoronto/page-templates/home.php
@@ -0,0 +1,345 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ */
+
+get_header(); ?>
+
+    <section class="tp-banner-container">
+    <div class="tp-banner" >
+    <ul>	<!-- SLIDE  -->
+    <li data-transition="fade" data-slotamount="4" data-masterspeed="1500" >
+        <!-- MAIN IMAGE -->
+        <img src="<?php echo get_template_directory_uri() ?>/sliderimages/slide_5.jpg" alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+        <!-- LAYERS -->
+
+        <!-- LAYER NR. 1 -->
+        <div class="tp-caption skewfromrightshort customout"
+             data-x="center"
+             data-y="center"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="1200"
+             data-easing="Power4.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 2"><img src="<?php echo get_template_directory_uri() ?>/sliderimages/logo.png" alt="">
+        </div>
+
+        <!-- LAYER NR. 2 -->
+        <div class="tp-caption medium_bg_darkblue skewfromleft customout"
+             data-x="left"
+             data-y="190"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1500"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 6">PHP Working Subscription
+        </div>
+
+        <!-- LAYER NR. 3 -->
+        <div class="tp-caption medium_bg_darkblue skewfromleft customout"
+             data-x="left"
+             data-y="245"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1500"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 6">PHP Working wizard
+        </div>
+
+        <!-- LAYER NR. 3 -->
+        <div class="tp-caption medium_bg_darkblue skewfromleft customout"
+             data-x="left"
+             data-y="300"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1500"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 6">PHP Working newsletter
+        </div>
+
+        <!-- LAYER NR. 4 -->
+        <div class="tp-caption medium_bg_darkblue skewfromright customout"
+             data-x="right"
+             data-y="190"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1800"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 8">Four Home pages
+        </div>
+
+        <!-- LAYER NR. 5 -->
+        <div class="tp-caption medium_bg_darkblue skewfromright customout"
+             data-x="right"
+             data-y="245"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1800"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 8">Course timeline
+        </div>
+
+        <!-- LAYER NR. 3 -->
+        <div class="tp-caption medium_bg_darkblue skewfromleft customout"
+             data-x="right"
+             data-y="300"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="800"
+             data-start="1800"
+             data-easing="Power4.easeOut"
+             data-endspeed="300"
+             data-endeasing="Power1.easeIn"
+             data-captionhidden="on"
+             style="z-index: 6">20 HTML pages
+        </div>
+    </li>
+    <!-- SLIDE  -->
+    <li data-transition="zoomout" data-slotamount="4" data-masterspeed="1000" >
+        <!-- MAIN IMAGE -->
+        <img src="<?php echo get_template_directory_uri() ?>/sliderimages/slide_6.jpg" alt="slidebg2"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+        <!-- LAYERS -->
+
+        <!-- LAYER NR. 1 -->
+        <div class="tp-caption medium_light_white skewfromrightshort customout"
+             data-x="80"
+             data-y="96"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="800"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 4">Enjoy
+        </div>
+
+        <!-- LAYER NR. 2 -->
+        <div class="tp-caption medium_thin_white skewfromleftshort customout"
+             data-x="235"
+             data-y="110"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="900"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 5">&
+        </div>
+
+        <!-- LAYER NR. 3 -->
+        <div class="tp-caption large_bold_white skewfromleftshort customout"
+             data-x="80"
+             data-y="152"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="300"
+             data-start="1100"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 7">Share your skills
+        </div>
+
+        <!-- LAYER NR. 4 -->
+        <div class="tp-caption small_thin_white  customin customout"
+             data-x="80"
+             data-y="240"
+             data-customin="x:0;y:100;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:1;scaleY:3;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:0% 0%;"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="1300"
+             data-easing="Power4.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 8">Slider Revolution is the highly acclaimed<br/> slide-based displaying solution, thousands of<br/> businesses, theme developers and everyday<br/> people use and love!
+        </div>
+    </li>
+
+    <!-- SLIDE  -->
+    <li data-transition="cube-horizontal" data-slotamount="4" data-masterspeed="1000" >
+        <!-- MAIN IMAGE -->
+        <img src="<?php echo get_template_directory_uri() ?>/sliderimages/slide_1.jpg" alt="slidebg3"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+        <!-- LAYERS -->
+
+        <!-- LAYER NR. 1 -->
+        <div class="tp-caption medium_light_white skewfromrightshort customout"
+             data-x="80"
+             data-y="96"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="800"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 4">Enjoy
+        </div>
+
+        <!-- LAYER NR. 2 -->
+        <div class="tp-caption medium_thin_white skewfromleftshort customout"
+             data-x="235"
+             data-y="110"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="900"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 5">&
+        </div>
+
+        <!-- LAYER NR. 3 -->
+        <div class="tp-caption large_bold_white skewfromleftshort customout"
+             data-x="80"
+             data-y="152"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="300"
+             data-start="1100"
+             data-easing="Back.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 7">Improve your skills
+        </div>
+
+        <!-- LAYER NR. 4 -->
+        <div class="tp-caption small_thin_white  customin customout"
+             data-x="80"
+             data-y="240"
+             data-customin="x:0;y:100;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:1;scaleY:3;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:0% 0%;"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="500"
+             data-start="1300"
+             data-easing="Power4.easeOut"
+             data-endspeed="500"
+             data-endeasing="Power4.easeIn"
+             data-captionhidden="on"
+             style="z-index: 8">Slider Revolution is the highly acclaimed<br/> slide-based displaying solution, thousands of<br/> businesses, theme developers and everyday<br/> people use and love!
+        </div>
+    </li>
+
+    <!-- SLIDE  -->
+    <li data-transition="zoomin" data-slotamount="4" data-masterspeed="1000" >
+        <!-- MAIN IMAGE -->
+        <img src="<?php echo get_template_directory_uri() ?>/sliderimages/slide_7.jpg" alt="slidebg4"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+        <!-- LAYERS -->
+
+        <!-- LAYER NR. 10 -->
+        <div class="tp-caption large_bold_white customin customout"
+             data-x="center" data-hoffset="0"
+             data-y="160"
+             data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="1000"
+             data-start="1700"
+             data-easing="Back.easeInOut"
+             data-endspeed="300"
+             style="z-index: 11">Single or multi course
+        </div>
+
+        <!-- LAYER NR. 11 -->
+        <div class="tp-caption mediumlarge_light_white_center customin customout"
+             data-x="center" data-hoffset="0"
+             data-y="bottom" data-voffset="-150"
+             data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+             data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+             data-speed="1000"
+             data-start="1900"
+             data-easing="Back.easeInOut"
+             data-endspeed="300"
+             style="z-index: 12">Lorem ipsum dolor sit amet,<br/>
+            civibus efficiantur in<br/>
+            id tempor imperdiet deterruisse.
+        </div>
+    </li>
+
+    </ul>
+    <div class="tp-bannertimer"></div>
+    </div>
+    </section><!-- End slider -->
+
+
+
+    <section id="main-features">
+        <div class="divider_top_black"></div>
+        <div class="container">
+            <div class="row">
+                <div class=" col-md-10 col-md-offset-1 text-center">
+                    <h2><?php bloginfo( 'description' ) ?></h2>
+                    <p class="lead">
+                        Lorem ipsum dolor sit amet, ius minim gubergren ad. <br>
+                        At mei sumo sonet audiam, ad mutat elitr platonem vix. Ne nisl idque fierent vix. Ferri clitaponderum ne.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="feature">
+                        <i class="icon-trophy"></i>
+                        <h3>Expert teachers</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature">
+                        <i class=" icon-ok-4"></i>
+                        <h3>Trusted certifications</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature">
+                        <i class="icon-mic"></i>
+                        <h3>+500 Audio lessons</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature">
+                        <i class="icon-video"></i>
+                        <h3>+1.200 Video lessons</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
+                        </p>
+                    </div>
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container-->
+    </section><!-- End main-features -->
+
+
+
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
